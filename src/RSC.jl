caaa038d159f2f175593d9994f1f2c963e61abf0
Douglas Bates <dmbates@gmail.com>
2013-02-26 09:23:38 -0600

    Interim changes for mixed-effects models

--- src/RSC.jl
+++ src/RSC.jl
@@ -4,20 +4,29 @@
 ## the row indices.  This requires that the number of nonzeros in each
 ## column be constant.  The row indices should be sorted within columns
 
-require("sparse.jl")
-import Base.*                           # to define a new method
+## To allow for both random effects and fixed-effects terms in a mixed-effects
+## model the non-zero values can have more rows than the row indices, the
+## convention being that the p extra rows are dense rows appended to the matrix.
+
+import Base.(*)
+import Base.convert
+import Base.copy
+import Base.nnz
+import Base.show
+import Base.size
+import Base.SparseMatrixCSC
 
 type SparseMatrixRSC{Tv,Ti<:Union(Int32,Int64)} <: AbstractSparseMatrix{Tv,Ti}
-    m::Int                              # number of rows
+    m::Int                              # number of rows in the Zt part
+    p::Int                              # number of rows in the Xt part
     rowval::Matrix{Ti}                  # row indices of nonzeros
     nzval::Matrix{Tv}                   # nonzero values
     function SparseMatrixRSC(rowval::Matrix{Ti},nzval::Matrix{Tv})
-        m,n = size(rowval)
-        if size(nzval) != size(rowval)
-            error("sizes of nzval, $(size(nzval)), and rowval, $(size(rowval)), must match")
+        if size(nzval,2) != size(rowval,2)
+            error("number of columns in nzval, $(size(nzval,2)), should be $(size(rowval,2))")
         end
         if !all(rowval .> 0) error("row values must be positive") end
-        new(int(max(rowval)), rowval, nzval)
+        new(int(max(rowval)), size(nzval,1) - size(rowval,1), rowval, nzval)
     end
 end
 
@@ -31,6 +40,8 @@ size(A::SparseMatrixRSC) = (A.m, size(A.nzval,2))
 size(A::SparseMatrixRSC,d) = d == 1 ? A.m : size(A.nzval,d)
 copy(A::SparseMatrixRSC) = SparseMatrixRSC(copy(A.rowval), copy(A.nzval))
 
+mktheta(nc) = mapreduce(j->mapreduce(k->float([1.,zeros(j-k)]), vcat, 1:j), vcat, nc)
+
 function convert{Tv,Ti}(::Type{SparseMatrixCSC}, x::SparseMatrixRSC{Tv,Ti})
     k,n = size(x.rowval)
     m   = int(max(x.rowval))
@@ -43,7 +54,7 @@ end
 
 convert{Tv}(::Type{Matrix}, x::SparseMatrixRSC{Tv}) = convert(Matrix{Tv}, convert(SparseMatrixCSC, x))
 
-function show(io, A::SparseMatrixRSC)
+function show(io::IO, A::SparseMatrixRSC)
     println(io, "$(A.m) by $(size(A.nzval, 2)) regular sparse column matrix")
     println(io, "Row indices: ", A.rowval)
     print(io, "Non-zero values: ", A.nzval)
@@ -87,12 +98,12 @@ function tcrossprodPat{Tv,Ti}(A::SparseMatrixRSC{Tv,Ti}, dd)
             ss = vv[jj]
             for l in k:kk
                 ll = Ar[l,j]
-                if !has(ss, ll) add(ss, ll) end
+                if !has(ss, ll) add!(ss, ll) end
             end
         end
     end
     colptr = one(Ti) + vcat(zero(Ti), cumsum([convert(Ti,length(s)) for s in vv]))
-    rowval = mapreduce(vcat, s->sort!(elements(s)), vv)
+    rowval = mapreduce(s->sort!(elements(s)), vcat, vv)
     aat = SparseMatrixCSC{Tv,Ti}(m, m, colptr, rowval, zeros(Tv, (length(rowval,))))
     for d in dd, i in colptr[d]:(colptr[d+1] - 1)
         if rowval[i] == d
@@ -103,10 +114,17 @@ function tcrossprodPat{Tv,Ti}(A::SparseMatrixRSC{Tv,Ti}, dd)
     aat
 end
 
+
+function mktheta(nc)
+   {(a=eye(j);mapreduce(i->a[1:i,i], vcat, 1:j)) for j in nc}
+end
+    
+
 tcrossprodPat{Tv,Ti}(A::SparseMatrixRSC{Tv,Ti}) = tcrossprodPat(A, Array(Ti, (0,)))
 
 ## DyeStuff example from the lme4 package for R
-#ZXt = SparseMatrixRSC(int32(hcat(GLM.gl(6,5), 7.*ones(Int, 30)))', ones(Float64,(2,30)))
+# using DataFrames
+#ZXt = SparseMatrixRSC(int32(gl(6,5).refs)', ones(Float64,(2,30)))
 #ZXtZX = tcrossprodPat(ZXt,1:6)
 #tcrossprod!(ZXt, copy(ZXtZX))
 #Yield = float([1545, 1440, 1440, 1520, 1580, 1540, 1555, 1490, 1560, 1495, 1595, 1550, 1605, 1510, 1560, 1445, 1440, 1595, 1465, 1545, 1595, 1630, 1515, 1635, 1625, 1520, 1455, 1450, 1480, 1445])
