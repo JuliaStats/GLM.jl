70395550bf33ccd7179d0bfde78312d6578c88e1
Douglas Bates <dmbates@gmail.com>
2013-03-29 13:30:53 -0500

    Added fit methods and simpler external constructors for LMMsimple

--- src/LMM.jl
+++ src/LMM.jl
@@ -1,7 +1,6 @@
 module LMM                              # linear mixed models
 
-using RSC
-using Base.LinAlg.CHOLMOD.CholmodDense
+using RSC, NLopt
 using Base.LinAlg.CHOLMOD.CholmodFactor
 using Base.LinAlg.CHOLMOD.CholmodSparse
 using Base.LinAlg.CHOLMOD.chm_scale!
@@ -9,13 +8,16 @@ using Base.LinAlg.CHOLMOD.chm_factorize!
 
 export LMMsimple
 import Distributions.deviance
+import Distributions.fit
 import Base.fill!
 
+typealias Inds Union(Int, Vector{Int}, Range{Int}, Range1{Int})
 typealias ITypes Union(Int32,Int64)
 
 type LMMsimple{Tv<:Float64,Ti<:ITypes}
     ZXt::SparseMatrixRSC{Tv,Ti}# model matrices Z and X in an RSC structure
     theta::Vector{Tv}          # variance component parameter vector
+    lower::Vector{Tv}          # lower bounds (always zeros(length(theta)) for these models)
     A::CholmodSparse{Tv,Ti}    # sparse symmetric system matrix
     anzv::Vector{Tv}           # cached copy of nonzeros from ZXt*ZXt'
     L::CholmodFactor{Tv,Ti}    # factor of current system matrix
@@ -27,18 +29,20 @@ type LMMsimple{Tv<:Float64,Ti<:ITypes}
     mu::Vector{Tv}             # fitted response vector
 end
 
-## Add a q by q identity block to the upper left of a symmetric A stored in the upper triangle 
-function pluseye{Tv}(A::CholmodSparse{Tv}, q::Integer) 
+## Add an identity block on Inds to a symmetric A stored in the upper triangle 
+function pluseye{T}(A::CholmodSparse{T}, inds::Inds) 
     if A.c.stype <= 0 error("Matrix A must be symmetric and stored in upper triangle") end
     cp = A.colptr0
     rv = A.rowval0
     xv = A.nzval
-    for j in 1:q
+    for j in inds
         k = cp[j+1]
         assert(rv[k] == j-1)
-        xv[k] += one(Tv)
+        xv[k] += one(T)
     end
 end
+
+pluseye(A::CholmodSparse) = pluseye(A,1:size(A,1))
 
 function LMMsimple{Tv<:Float64,Ti<:ITypes}(ZXt::SparseMatrixRSC{Tv,Ti},
                                            y::Vector{Tv},wts::Vector{Tv})
@@ -48,19 +52,37 @@ function LMMsimple{Tv<:Float64,Ti<:ITypes}(ZXt::SparseMatrixRSC{Tv,Ti},
     if lwts != 0 && lwts != n error("length(wts) = $lwts should be 0 or length(y) = $n") end
     A = A_mul_Bc(ZXt,ZXt) # i.e. ZXt*ZXt' (single quotes confuse syntax highlighting)
     anzv = copy(A.nzval)
-    pluseye(A,ZXt.q)
+    pluseye(A, 1:ZXt.q)
     L = cholfact(A)
     ZXty = ZXt*y
     ubeta = vec((L\ZXty).mat)
-    LMMsimple{Tv,Ti}(ZXt, ones(size(ZXt.rowval,1)), A, anzv, L,
+    k = size(ZXt.rowval, 1)
+    LMMsimple{Tv,Ti}(ZXt, ones(k), zeros(k), A, anzv, L,
                      ubeta, sqrt(wts), y, ZXty, ones(size(L,1)), Ac_mul_B(ZXt,ubeta))
 end
 
-function fill!{T,Ti<:Union(Vector{Int}, Range{Int}, Range1{Int})}(a::Vector{T}, x::T, inds::Ti)
+LMMsimple(ZXt,y) = LMMsimple(ZXt, y, Array(eltype(y), 0))
+
+function LMMsimple{Tv<:Float64,Ti<:ITypes}(inds::Matrix{Ti},
+                                           X::Matrix{Tv},
+                                           y::Vector{Tv},
+                                           wts::Vector{Tv})
+    n = length(y)
+    if !(size(inds,1) == size(X,1) == n) error("Dimension mismatch") end
+    ii = copy(inds)
+    for j in 2:size(ii,2) ii[:,j] += max(ii[:,j-1]) end
+    LMMsimple(SparseMatrixRSC(ii', [ones(size(ii)) X]'), y, wts)
+end
+
+function LMMsimple{Tv<:Float64,Ti<:ITypes}(inds::Matrix{Ti}, X::Matrix{Tv}, y::Vector{Tv})
+    LMMsimple(inds, X, y, Array(Tv, 0))
+end
+
+function fill!{T,Ti<:Inds}(a::Vector{T}, x::T, inds::Ti)
     for i in inds a[i] = x end
 end
 
-function deviance(m::LMMsimple,theta::Vector{Float64})
+function deviance(m::LMMsimple,theta::Vector{Float64}, REML::Bool)
     if length(theta) != length(m.theta) error("Dimension mismatch") end
     if any(theta .< 0.) error("all elements of theta must be non-negative") end
     m.theta[:] = theta[:]               # copy in place
@@ -69,22 +91,23 @@ function deviance(m::LMMsimple,theta::Vector{Float64})
     q = ZXt.q
     p = ZXt.p
     for i in 1:length(theta) fill!(m.lambda, theta[i], int(ZXt.rowrange[i])) end
-    ## FIXME Add a chm_scale! method to the Base.LinAlg.CHOLMOD module
-    chm_scale!(m.A, CholmodDense(m.lambda), 3) # symmetric scaling
-    pluseye(m.A, q)
+    chm_scale!(m.A, m.lambda, 3) # symmetric scaling
+    pluseye(m.A, 1:q)
     chm_factorize!(m.L,m.A)
     m.ubeta[:] = vec((m.L \ (m.lambda .* m.ZXty)).mat)[:]
     m.mu[:] = Ac_mul_B(ZXt, m.lambda .* m.ubeta)
     rss = (s = 0.; for r in (m.y - m.mu) s += r*r end; s)
-    ldL2 = logdet(m.L, 1:q)
-    ldA = logdet(m.L)
     ussq = (s = 0.; for j in 1:q s += square(m.ubeta[j]) end; s)
     lnum = log(2pi * (rss + ussq))
+    if REML
+        nmp = float(size(ZXt,2) - p)
+        return logdet(m.L) + nmp * (1 + lnum - log(nmp))
+    end
     n = float(size(ZXt,2))
-    nmp = n - p
-    [ldL2 + n * (1 + lnum - log(n)), ldA + nmp * (1 + lnum - log(nmp))]
+    return logdet(m.L, 1:q) + n * (1 + lnum - log(n))
 end
 
+deviance(m::LMMsimple,theta::Vector{Float64}) = deviance(m, theta, false)
 # Dyestuff example
 # ZXt = SparseMatrixRSC(1+div([0:29],5)), ones((2,30))
 # Yield = [1545., 1440, 1440, 1520, 1580, 1540, 1555, 1490, 1560,
@@ -93,4 +116,33 @@ end
 #                 1450, 1480, 1445]
 # mm = LMMsimple(ZXt)
 
+function fit(mm::LMMsimple, REML::Bool, verbose::Bool) # keyword arguments will help
+    k = length(mm.theta)
+    opt = Opt(:LN_BOBYQA, k)
+    ftol_abs!(opt, 1e-6)    # criterion on deviance changes
+    xtol_abs!(opt, 1e-6)    # criterion on all parameter value changes
+    lower_bounds!(opt, mm.lower)
+    function obj(x::Vector{Float64}, g::Vector{Float64})
+        if length(g) > 0 error("gradient evaluations are not provided") end
+        deviance(mm, x, REML)
+    end
+    if verbose
+        count = 0
+        function vobj(x::Vector{Float64}, g::Vector{Float64})
+            count += 1
+            val = obj(x, g)
+            println("f_$count: $val, $x")
+            val
+        end
+        min_objective!(opt, vobj)
+    else
+        min_objective!(opt, obj)
+    end
+    optimize(opt, mm.theta)
 end
+
+fit(mm::LMMsimple, REML::Bool) = fit(mm, REML, false)
+fit(mm::LMMsimple) = fit(mm, false, false)
+
+end
+
