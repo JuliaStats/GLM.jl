fd813cad1f00f80c815b61be3c27dbe82521e85c
Douglas Bates <dmbates@gmail.com>
2013-08-03 12:55:53 -0500

    Use a small glm fit to force compilation.

--- perf/glm.jl
+++ perf/glm.jl
@@ -1,4 +1,5 @@
 using GLM
+glm(:(y ~ 1), DataFrame(y = float64(randbool(10))), Binomial())
 
 n = 2_500_000; srand(1234321)
 df2 = DataFrame(x1 = rand(Normal(), n),
@@ -9,4 +10,6 @@ eta = ModelMatrix(ModelFrame(Formula(:(~ (x1 + x2 + ss))), df2)).m * beta;
 mu = linkinv!(LogitLink(), similar(eta), eta);
 df2["y"] = float64(rand(n) .< mu); df2["eta"] = eta; df2["mu"] = mu;
 head(df2)
+gc()
+gc()
 @time gm6 = glm(:(y ~ x1 + x2 + ss), df2, Binomial(), LogitLink())
