c8085b51ff0038d17854bb45833bc0dc6734de33
Calder Coalson <caldercoalson@gmail.com>
2014-02-11 11:46:53 -0600

    Replaced a reshape with a transpose

--- src/lm.jl
+++ src/lm.jl
@@ -72,7 +72,7 @@ function predict(mm::LmMod, newx::Matrix)
 end
 
 function predict(mm::LmMod, xs...)
-    predict(mm, reshape([1.,xs...],1,length(xs)+1))[1]
+    predict(mm, transpose([1.,xs...]))[1]
 end
 
 function confint(obj::LmMod, level::Real)
