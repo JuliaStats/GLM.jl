8cc97bb5326b5b8dfbed4f305d6c1c0ae8b8c8fd
timema <taylor.maxwell@gmail.com>
2014-05-15 20:37:14 -0600

    add effects and ANOVA test
    the effects function extracts the effects from an LmMod either produced with a formula and dataframe or from LmMod produced using a y and X matrix.  ANOVA test allows you to specify terms in an LmMod model to do an anova (full vs reduced) test.  You can specify 1 or more terms if the model was produced with a formula and data frame.  If the LmMod was produced with a y and X, you can specify the columns of X that you would like to group together.  ANOVAtest is a type to hold DF,SS,MS,F, and either the pval or -log10pval.  It has a show function that produces a table.  Below are some examples.
    
    a=randn(100,4);
    b=convert(DataFrame,a);
    
    c=fit(LmMod,x1~x2+x3+x4,b)   #fit model with formula and dataframe
    d=ANOVAtest(c,[2,3],log10pval=true)  #test of multiple columns of X with LmMod made with formula and dataframe
    e=ANOVAtest(c,3)     #test of a single column of X with LmMod made with formula and dataframe
    f=ANOVAtest(c,[3])    #shows that you can also do one term with a single cell array
    
    g=fit(LmMod,c.model.pp.X,c.model.rr.y)   #fit model with X and y without formula or dataframe
    h=ANOVAtest(g,[3,4])  #test of multiple columns of X with LmMod made without formula and dataframe
    i=ANOVAtest(g,4)   #test of a single column of X with LmMod made without formula and dataframe
    j=ANOVAtest(g,[4])    #shows that you can also do one term was a single cell array

--- src/lm.jl
+++ src/lm.jl
@@ -63,3 +63,102 @@ function confint(obj::LmMod, level::Real)
     quantile(TDist(df_residual(obj)), (1. - level)/2.) * [1. -1.]
 end
 confint(obj::LmMod) = confint(obj, 0.95)
+
+effects(mod::RegressionModel)=(mod.model.pp.qr[:Q]'*mod.model.rr.y)[1:size(mod.model.pp.X,2)]
+effects(mod::LmMod{DensePredQR{Float64}})=(mod.pp.qr[:Q]'*mod.rr.y)[1:size(mod.pp.X,2)]
+
+type ANOVAtest
+    SSH::Float64
+    SSE::Float64
+    MSH::Float64
+    MSE::Float64
+    dfH::Int
+    dfE::Int
+    fstat::Float64
+    pval::Float64    #regular pvalue or -log10pval
+    log10pval::Bool
+end
+
+#this is a test for a group of terms from a LmMod derived from a formula and dataframe
+function ANOVAtest(mod::RegressionModel,terms::Array{Int,1}; log10pval=false)
+    #terms is arrary of number for each term in the model to be tested together, starting with the intercept at 0
+    eff=effects(mod)  #get effect for each coefficient
+    ind=findin(mod.mm.assign,terms)
+    eff=eff[ind]
+    SSH=sum(Abs2Fun(),eff)
+    dfH=length(eff)
+    MSH=SSH/dfH
+    SSE=deviance(mod.model.rr)
+    dfE=df_residual(mod.model.pp)
+    MSE=SSE/dfE
+    fstat=MSH/MSE
+    pval=ccdf(FDist(dfH, dfE), fstat)
+    if log10pval pval= -log10(pval) end
+    return ANOVAtest(SSH,SSE,MSH,MSE,dfH,dfE,fstat,pval,log10pval)
+end
+
+#this is a test for a single term from a LmMod derived from a formula and dataframe
+function ANOVAtest(mod::RegressionModel,term::Int; log10pval=false)
+    #term an integer for a single term in model to be tested, starting with the intercept at 0
+    eff=effects(mod)  #get effect for each coefficient
+    ind=findin(mod.mm.assign,term)
+    eff=eff[ind]
+    SSH=eff[1]*eff[1]
+    dfH=length(eff)
+    MSH=SSH/dfH
+    SSE=deviance(mod.model.rr)
+    dfE=df_residual(mod.model.pp)
+    MSE=SSE/dfE
+    fstat=MSH/MSE
+    pval=ccdf(FDist(dfH, dfE), fstat)
+    if log10pval pval= -log10(pval) end
+    return ANOVAtest(SSH,SSE,MSH,MSE,dfH,dfE,fstat,pval,log10pval)
+end
+
+
+#this is a test for a group of coefficients/columns of X based on a LmMod derived without a formula and dataframe
+function ANOVAtest(mod::LmMod{DensePredQR{Float64}},cols::Array{Int,1}; log10pval=false)
+    #cols a vector of position numbers (column number of X) to be grouped together with the intercept starting at 1
+    #this does not refer the terms of a model defined by a formula if a term has >1 DF
+    eff=effects(mod)  #get effect for each coefficient
+    eff=eff[cols]
+    SSH=sum(Abs2Fun(),eff)
+    dfH=length(eff)
+    MSH=SSH/dfH
+    SSE=deviance(mod.rr)
+    dfE=df_residual(mod.pp)
+    MSE=SSE/dfE
+    fstat=MSH/MSE
+    pval=ccdf(FDist(dfH, dfE), fstat)
+    if log10pval pval= -log10(pval) end
+    return ANOVAtest(SSH,SSE,MSH,MSE,dfH,dfE,fstat,pval,log10pval)
+end
+
+#this is a test for a single coefficient/columns of X based on a LmMod derived without a formula and dataframe
+function ANOVAtest(mod::LmMod{DensePredQR{Float64}},col::Int; log10pval=false)
+    #col means the position number of the column of X with the intercept starting at 1
+    #this does not refer the terms of a model defined by a formula if a term has >1 DF
+    eff=effects(mod)  #get effect for each coefficient
+    eff=eff[col]
+    SSH=eff[1]*eff[1]
+    dfH=length(eff)
+    MSH=SSH/dfH
+    SSE=deviance(mod.rr)
+    dfE=df_residual(mod.pp)
+    MSE=SSE/dfE
+    fstat=MSH/MSE
+    pval=ccdf(FDist(dfH, dfE), fstat)
+    if log10pval pval= -log10(pval) end
+    return ANOVAtest(SSH,SSE,MSH,MSE,dfH,dfE,fstat,pval,log10pval)
+end
+
+function Base.show(io::IO,at::ANOVAtest)
+    if at.log10pval
+        println("              ","DF",'\t',"SS",'\t',"MS",'\t',"F",'\t',"log10pval")
+    else
+        println("              ","DF",'\t',"SS",'\t',"MS",'\t',"F",'\t',"pval")
+    end
+    println("Hypothesis    ",round(at.dfH,3),'\t',round(at.SSH,3),'\t',round(at.MSH,3),'\t',round(at.fstat,3),'\t',at.pval)
+    println("Residuals     ",round(at.dfE,3),'\t', round(at.SSE,3),'\t', round(at.MSE,3))
+end
+
