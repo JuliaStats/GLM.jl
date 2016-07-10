a772ce48b0e1fed196d5d0a7dacd6e3776b95777
Alex Arslan <ararslan@comcast.net>
2016-07-10 13:13:18 -0700

    Update functions deprecated in StatsBase (#146)
    * Update functions deprecated in StatsBase
    * Bump DataFrames version requirement for testing

--- src/GLM.jl
+++ src/GLM.jl
@@ -8,8 +8,8 @@ module GLM
     using Distributions: sqrt2, sqrt2π
 
     import Base: (\), cholfact, convert, cor, show, size
-    import StatsBase: coef, coeftable, confint, deviance, nulldeviance, df, df_residual, loglikelihood, nullloglikelihood, nobs, stderr, vcov, residuals, predict, fit, model_response, xlogy, R2, R², adjR2, adjR²
-    export coef, coeftable, confint, deviance, nulldeviance, df, df_residual, loglikelihood, nobs, stderr, vcov, residuals, predict, fit, fit!, model_response, R2, R², adjR2, adjR²
+    import StatsBase: coef, coeftable, confint, deviance, nulldeviance, df, df_residual, loglikelihood, nullloglikelihood, nobs, stderr, vcov, residuals, predict, fit, model_response, xlogy, r2, r², adjr2, adjr²
+    export coef, coeftable, confint, deviance, nulldeviance, df, df_residual, loglikelihood, nobs, stderr, vcov, residuals, predict, fit, fit!, model_response, r2, r², adjr2, adjr²
 
     export                              # types
         CauchitLink,
