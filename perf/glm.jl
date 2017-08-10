8b46d684d5ca014a715045204e1a9cc979bf3778
Douglas Bates <dmbates@gmail.com>
2017-08-10 12:48:03 -0500

    Clean up performance test.

--- perf/glm.jl
+++ perf/glm.jl
@@ -1,21 +1,21 @@
-using GLM, DataFrames
-glm(@formula(y ~ 1), DataFrame(y = float(bitrand(10))), Binomial())
+using GLM, DataFrames, StatsFuns
+glm(@formula(y ~ 1), DataFrame(y = float(bitrand(10))), Bernoulli())
 
-n = 2_500_000; srand(1234321)
-df2 = DataFrame(x1 = rand(Normal(), n),
+n = 2_500_000
+srand(1234321)
+const df2 = DataFrame(x1 = rand(Normal(), n),
                 x2 = rand(Exponential(), n),
                 ss = pool(rand(DiscreteUniform(50), n)));
-mf = ModelFrame(@formula(x1 ~ x1 + x2 + ss), df2)
-mm = ModelMatrix(mf)
-beta = unshift!(rand(Normal(),52), 0.5); # "true" parameter values
+const mf = ModelFrame(@formula(x1 ~ x1 + x2 + ss), df2)
+const mm = ModelMatrix(mf)
+const β = unshift!(rand(Normal(),52), 0.5); # "true" parameter values
 
 ## Create linear predictor and mean response
-eta = mm.m * beta;
-mu = [linkinv(LogitLink(), x) for x in eta]
-y = Float64[rand() < μ for μ in mu];        # simulate observed responses
 
-gm6 = glm(mm.m, y, Binomial())
-@time glm(mm.m, y, Binomial());
+const y = Float64[rand() < logistic(η) for η in mm.m * β];        # simulate observed responses
+
+gm6 = glm(mm.m, y, Bernoulli())
+@time glm(mm.m, y, Bernoulli());
 #@profile glm(mm.m, y, Binomial());
 #using ProfileView
 #ProfileView.view()
