ecd2dfbc895fe4dd2ba47aa0fe49130f7bc81ee6
Andreas Noack <andreasnoackjensen@gmail.com>
2018-05-08 21:32:55 +0200

    Enable Documenter hosting

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,6 @@ makedocs(
 deploydocs(
     repo   = "github.com/JuliaStats/GLM.jl.git",
     julia  = "0.6",
-    osname = "linux",
     target = "build",
     deps   = nothing,
     make   = nothing
