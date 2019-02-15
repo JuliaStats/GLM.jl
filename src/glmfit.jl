b24a28175cb6316080be5b12cca3423af0e48f65
Milan Bouchet-Valat <nalimilan@club.fr>
2019-02-15 14:52:35 +0100

    Fix docstring for dispersion
    σ² is only returned when sqr=true.

--- src/glmfit.jl
+++ src/glmfit.jl
@@ -337,7 +337,7 @@ Distributions.Distribution(m::GeneralizedLinearModel) = Distribution(m.rr)
     dispersion(m::AbstractGLM, sqr::Bool=false)
 
 Return the estimated dispersion (or scale) parameter for a model's distribution,
-generally written σ² for linear models and ϕ for generalized linear models.
+generally written σ for linear models and ϕ for generalized linear models.
 It is, by definition, equal to 1 for the Bernoulli, Binomial, and Poisson families.
 
 If `sqr` is `true`, the squared dispersion parameter is returned.
