96763f1c819332155e0395606c01fb6c89817569
Thibaut Lienart <ltib@me.com>
2019-07-19 12:23:22 +0100

    Typo fix
    Wiebull -> Weibull

--- src/glmtools.jl
+++ src/glmtools.jl
@@ -27,7 +27,7 @@ mutable struct CauchitLink <: Link01 end
 """
     CloglogLink
 
-A [`Link01`](@ref) corresponding to the extreme value (or log-Wiebull) distribution.  The
+A [`Link01`](@ref) corresponding to the extreme value (or log-Weibull) distribution.  The
 link is the complementary log-log transformation, `log(1 - log(-μ))`.
 """
 mutable struct CloglogLink  <: Link01 end
