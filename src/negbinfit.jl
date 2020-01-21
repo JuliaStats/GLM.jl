6881e588b117cac8bc2b0010addb5b326b98787d
Milan Bouchet-Valat <nalimilan@club.fr>
2020-01-21 17:15:38 +0100

    Fix name

--- src/negbinfit.jl
+++ src/negbinfit.jl
@@ -33,7 +33,7 @@ function mle_for_θ(y::AbstractVector, μ::AbstractVector, wts::AbstractVector;
     if !converged
         info_msg = "Estimating dispersion parameter failed, which may " *
             "indicate Poisson distributed data."
-        throw(ConvergenceException(maxIter, NaN, NaN, info_msg))
+        throw(ConvergenceException(maxiter, NaN, NaN, info_msg))
     end
     θ
 end
