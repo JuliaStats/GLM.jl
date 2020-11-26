605777167ca940ae34bf7cbd0749e6d46b99f683
pdeffebach <23196228+pdeffebach@users.noreply.github.com>
2020-11-26 10:42:11 -0500

    whitespace fixes

--- src/lm.jl
+++ src/lm.jl
@@ -28,10 +28,10 @@ mutable struct LmResp{V<:FPVector} <: ModResp  # response in a linear model
     end
 end
 
-LmResp(y::FPVector, wts::FPVector=similar(y, 0)) =
+LmResp(y::FPVector, wts::FPVector=similar(y, 0)) = 
     LmResp{typeof(y)}(fill!(similar(y), 0), similar(y, 0), wts, y)
 
-LmResp(y::AbstractVector{<:Real}, wts::AbstractVector{<:Real}=similar(y, 0)) =
+LmResp(y::AbstractVector{<:Real}, wts::AbstractVector{<:Real}=similar(y, 0)) = 
     LmResp(float(y), float(wts))
 
 function updateμ!(r::LmResp{V}, linPr::V) where V<:FPVector
@@ -65,7 +65,7 @@ function nulldeviance(r::LmResp)
     wts = r.wts
     if isempty(wts)
         m = mean(y)
-    else
+    else 
         m = mean(r.y, weights(r.wts))
     end
 
@@ -89,7 +89,7 @@ end
 
 function nullloglikelihood(r::LmResp)
     n = isempty(r.wts) ? length(r.y) : sum(r.wts)
-    -n/2 * (log(2π * nulldeviance(r)/n) + 1)
+    -n/2 * (log(2π * nulldeviance(r)/n) + 1) 
 end
 
 residuals(r::LmResp) = r.y - r.mu
@@ -114,10 +114,10 @@ LinearAlgebra.cholesky(x::LinearModel) = cholesky(x.pp)
 function StatsBase.fit!(obj::LinearModel)
     if isempty(obj.rr.wts)
         delbeta!(obj.pp, obj.rr.y)
-    else
+    else 
         delbeta!(obj.pp, obj.rr.y, obj.rr.wts)
     end
-    installbeta!(obj.pp)
+    installbeta!(obj.pp)    
     updateμ!(obj.rr, linpred(obj.pp, zero(eltype(obj.rr.y))))
     return obj
 end
