6d13db3c0a880e0b5dee5167761a645cf6e8e21f
Eric QUERE <e_quere@yahoo.fr>
2021-03-27 00:57:56 +0100

    no weights

--- test/runtests.jl
+++ test/runtests.jl
@@ -59,10 +59,8 @@ end
         CooksDW=[0.7371619575, 30.979165951, 0.0112480511, 0.5168548491, 0.0600217986, 0.1270980264] )
     
     t_lm = lm(@formula(Y ~ XA + XB), st_df)
-    @show(r2(t_lm))
     @test isapprox(st_df.CooksD, cooksdistance(t_lm))
     # t_lm_w = lm(@formula(Y ~ XA + XB), st_df, wts = st_df.W)
-    # @show(r2(t_lm_w))
     # @test isapprox(st_df.CooksDW, cooksdistance(t_lm_w))
 end
 
