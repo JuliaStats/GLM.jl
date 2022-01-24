67e7bd5662da34b379941203cbaadf6293262189
piever <pietro.vertechi@protonmail.com>
2022-01-24 10:57:50 +0100

    clarify docstring

--- src/lm.jl
+++ src/lm.jl
@@ -236,7 +236,7 @@ end
 
 If `interval` is `nothing` (the default), return a vector with the predicted values
 for model `mm` and new data `newx`.
-Otherwise, return a vector with the predicted values as well as
+Otherwise, return a vector with the predicted values, as well as vectors with
 the lower and upper confidence bounds for a given `level` (0.95 equates alpha = 0.05).
 Valid values of `interval` are `:confidence` delimiting the  uncertainty of the
 predicted relationship, and `:prediction` delimiting estimated bounds for new data points.
