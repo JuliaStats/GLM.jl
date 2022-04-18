aef3048b74a61ddd75e6e2be9c7004cbf9163921
ayushpatnaikgit <ayushpatnaik@gmail.com>
2022-04-18 12:59:47 +0530

    Follwing alphabetical order in references

--- src/glmtools.jl
+++ src/glmtools.jl
@@ -6,8 +6,8 @@ An abstract type whose subtypes refer to link functions.
 GLM currently supports the following links:
 [`CauchitLink`](@ref), [`CloglogLink`](@ref), [`IdentityLink`](@ref),
 [`InverseLink`](@ref), [`InverseSquareLink`](@ref), [`LogitLink`](@ref),
-[`LogLink`](@ref), [`NegativeBinomialLink`](@ref), [`ProbitLink`](@ref),
-[`SqrtLink`](@ref), [`PowerLink`](@ref).
+[`LogLink`](@ref), [`NegativeBinomialLink`](@ref), [`PowerLink`](@ref), [`ProbitLink`](@ref),
+[`SqrtLink`](@ref).
 
 Subtypes of `Link` are required to implement methods for
 [`GLM.linkfun`](@ref), [`GLM.linkinv`](@ref), [`GLM.mueta`](@ref),
