295f001a4e27cb888a219dde27b3176ddef27142
Mousum Dutta <44145580+mousum-github@users.noreply.github.com>
2022-09-09 16:22:31 +0530

    Update src/glmfit.jl
    Thanks for your suggestion.
    
    Co-authored-by: Milan Bouchet-Valat <nalimilan@club.fr>

--- src/glmfit.jl
+++ src/glmfit.jl
@@ -443,7 +443,10 @@ const FIT_GLM_DOC = """
     for a list of built-in links).
 
     # Keyword Arguments
-    - `dropcollinear::Bool=false`: Controls whether or not lm accepts a model matrix which is less-than-full rank.
+    - `dropcollinear::Bool=false`: Controls whether or not `lm` accepts a model matrix which
+      is less-than-full rank. If `true` (the default), only the first of each set of
+      linearly-dependent columns is used. The coefficient for redundant linearly dependent columns is
+      `0.0` and all associated statistics are set to `NaN`.
     - `dofit::Bool=true`: Determines whether model will be fit
     - `wts::Vector=similar(y,0)`: Prior frequency (a.k.a. case) weights of observations.
       Such weights are equivalent to repeating each observation a number of times equal
