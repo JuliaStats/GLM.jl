450c1e45b570a44130241b65d9b42d7f12391df8
Mousum Dutta <44145580+mousum-github@users.noreply.github.com>
2022-11-14 10:52:45 +0530

    Update src/linpred.jl
    Co-authored-by: Milan Bouchet-Valat <nalimilan@club.fr>

--- src/linpred.jl
+++ src/linpred.jl
@@ -361,5 +361,5 @@ dof_residual(obj::LinPredModel) = nobs(obj) - dof(obj) + 1
 hasintercept(m::LinPredModel) = any(i -> all(==(1), view(m.pp.X , :, i)), 1:size(m.pp.X, 2))
 
 linpred_rank(x::LinPred) = length(x.beta0)
-linpred_rank(x::DensePredChol{<:Any, <:CholeskyPivoted}) = x.chol.rank
+linpred_rank(x::DensePredChol{<:Any, <:CholeskyPivoted}) = rank(x.chol)
 linpred_rank(x::DensePredQR{<:Any,<:QRPivoted}) = rank(x.qr.R)
