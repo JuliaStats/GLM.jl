613f5fe4ae35cebeb7fa9b3b4474d83bc521dc3d
Phillip Alday <me@phillipalday.com>
2022-12-08 16:41:02 -0600

    add code for pearson

--- src/glmfit.jl
+++ src/glmfit.jl
@@ -821,8 +821,7 @@ function checky(y, d::Binomial)
     return nothing
 end
 
-# need to add :pearson
-const _RESIDUAL_TYPES = [:deviance, :response, :working]
+const _RESIDUAL_TYPES = [:deviance, :pearson, :response, :working]
 
 """
     residuals(model::GeneralizedLinearModel; type=:deviance)
@@ -834,6 +833,8 @@ Supported values for `type` are:
   contribution to the deviance
 - `:response`: the difference between the observed and fitted values
 - `:working`: working residuals (used during the IRLS process)
+- `:pearson`: Pearson residuals, i.e., response residuals scaled
+    by the standard error of the response
 """
 function residuals(model::GeneralizedLinearModel; type=:deviance)
     type in _RESIDUAL_TYPES ||
@@ -846,6 +847,8 @@ function residuals(model::GeneralizedLinearModel; type=:deviance)
         # XXX I think this might be the same as
         # 2 * wrkresid, but I'm not 100% sure if that holds across families
         return sign.(response(model) .- fitted(model)) .* sqrt.(model.rr.devresid)
+    elseif type == :pearson
+        return  sign.(response(model) .- fitted(model)) .* sqrt.(model.rr.wrkwt) .* abs.(model.rr.wrkresid)
     elseif type == :working
         return model.rr.wrkresid
     else
