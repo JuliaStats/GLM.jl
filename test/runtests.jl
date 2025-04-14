59781e2a274952c8f236b6417988a8b2ccd3f0c7
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2025-04-14 08:49:51 +0200

    Update test/runtests.jl
    Co-authored-by: Ajinkya Kokandakar <ajinkya-k@users.noreply.github.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -2177,7 +2177,7 @@ end
         LinearModel, @formula(OptDen ~ Carb), form; wts=aweights(form.awts), method=:qr)
     lm1 = fit(LinearModel, @formula(OptDen ~ Carb + CarbC),
         form; wts=aweights(form.awts), method=:qr)
-    @test coef(lm0) == coef(lm1)[1:2]
+    @test coef(lm0) ≈ coef(lm1)[1:2]
     @test stderror(lm0) == stderror(lm1)[1:2]
     @test isnan(stderror(lm1)[3])
     lm0 = fit(
